eb3177c8dc66adaf173a27f31c5bfa5e5fd4a45b
isaiah <isotodev@gmail.com>
2020-12-17 16:37:29 -0600

    First Commit

--- PizzaWorld.Client/Program.cs
+++ PizzaWorld.Client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using PizzaWorld.Domain.Singleton;
+using PizzaWorld.Domain.Models;
 
 namespace PizzaWorld.Client
 {
@@ -6,7 +9,74 @@ namespace PizzaWorld.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var cs = ClientSingleton.Instance;
+           // Greeting();
+        }
+
+        static void Greeting()
+        {
+            
+            Console.WriteLine("Welcome to PizzaPalace");
+            Console.Write("Enter 1 if you are an Customer | ");
+            Console.Write("Enter 2 if you are an Employee \n");
+            CheckUserType();
+        }
+
+        static void CheckUserType()
+        {
+            try
+            {
+                string UserEntry = Console.ReadLine();
+                if (UserEntry != null)
+                {
+                    int UserResult = Int32.Parse(UserEntry);
+                    switch(UserResult)
+                    {
+                        case 1:
+                            Console.WriteLine("Welcome Customer!");
+                            break;
+                        case 2:
+                            Console.WriteLine("Welcome Employee!");
+                            break;
+                        default:
+                            Console.WriteLine("Please Enter a valid entry \n");
+                            Greeting();
+                            break;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Please Enter a Number \n");
+                Greeting();
+            }
+        }
+
+        static void CustomerCheckIn()
+        {
+            Console.Write("Enter 1 if you already have an account | ");
+            Console.Write("Enter 2 to create a new account \n");
+        }
+
+        static User CreateUser(string name)
+        {
+            return new User(name);
+        }
+
+        static List<Store> GetAllStores()
+        {
+            return new List<Store>()
+            {
+                new Store()
+            };
+        }
+
+        static void PrintAllStores()
+        {
+            foreach(var store in GetAllStores())
+            {
+                Console.WriteLine(store);
+            }
         }
     }
 }
